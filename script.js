c566edffab3727730a3efb0dd38dcc384a295be7
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-07-30 14:43:46 +1000

    Complete modal animations

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const openModal = document.querySelector(".openModal");
+const modal = document.querySelector(".modal");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.nodeName === "DIALOG") {
+    modal.setAttribute("closing", "");
+    modal.addEventListener(
+      "animationend",
+      () => {
+        modal.removeAttribute("closing");
+        modal.close();
+      },
+      { once: true }
+    );
+  }
+});
+
 let myLibrary = [];
 
 function addBookToLibrary() {}
