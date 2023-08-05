92bd6445d055311fc0fc45c7fa5391cfca636bef
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-08-05 14:28:29 +1000

    Add functions for adding books to array and display

--- script.js
+++ script.js
@@ -19,6 +19,46 @@ modal.addEventListener("click", (e) => {
   }
 });
 
+function bookMaker(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
 let myLibrary = [];
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  if (read == false) {
+    read = "notRead";
+  } else {
+    read = "read";
+  }
+  let newBook = new bookMaker(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+const submit = document.getElementById("submit");
+const container = document.getElementById("grid");
+
+submit.addEventListener("click", () => {
+  addBookToLibrary();
+  myLibrary.forEach((item) => {
+    const book = document.createElement("div");
+    book.classList = "book";
+    const card = `
+  <div class="book">
+    <p>${item.title}</p>
+    <p>${item.author}</p>
+    <p>${item.pages}</p>
+    <button class=${item.read}>Not read</button>
+    <button class="remove">Remove</button>
+  </div>
+`;
+    container.innerHTML += card;
+  });
+});
